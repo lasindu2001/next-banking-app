51d072250cf488fb5c40c130328cda9ca11befd3
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-03 23:50:22 +0530

    create home page UI

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,6 +1,16 @@
 const HeaderBox = ({ type = "title", title, subtext, user }: HeaderBoxProps) => {
   return (
-    <div>HeaderBox</div>
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">
+            &nbsp;{user}
+          </span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
   )
 }
 
