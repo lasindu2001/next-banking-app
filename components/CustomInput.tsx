afdb54b46d15603f2cbdcaed07da23994ffdb307
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-07 15:43:25 +0530

    create authform and custominput components

---
+++ components/CustomInput.tsx
@@ -0,0 +1,43 @@
+import { authFormSchema } from "@/lib/utils"
+import { Control, FieldPath } from "react-hook-form"
+import { z } from "zod"
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form"
+import { Input } from "./ui/input"
+
+const formSchema = authFormSchema('sign-up')
+
+interface CustomInput {
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className="form-item">
+                    <FormLabel className="form-label">
+                        {label}
+                    </FormLabel>
+                    <div className="flex w-full flex-col">
+                        <FormControl>
+                            <Input
+                                placeholder={placeholder}
+                                className="input-class"
+                                type={name === 'password' ? 'password' : 'text'}
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage className="form-message mt-2" />
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+export default CustomInput
