eff66994043b603d2fc4dd0503eaf688f138a9bc
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-07 22:26:19 +0530

    connect with appwrite and implement signup functionality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -35,7 +35,19 @@ const AuthForm = ({ type }: { type: string }) => {
         try {
             // Sign up with Appwrite & create plaid token
             if (type === 'sign-up') {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+                const newUser = await signUp(userData);
                 setUser(newUser);
             }
             if (type === 'sign-in') {
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,17 +1,37 @@
 'use server'
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async ({ email, password }: signInProps) => {
     try {
-        
+
     } catch (error) {
         console.error('Error', error);
     }
 }
 
-export const signUp = async ({ password, ...userData }: SignUpParams) => {
+export const signUp = async ({ ...userData }: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
     try {
-        
+        const { account } = await createAdminClient();
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            `${firstName} ${lastName}`
+        );
+        const session = await account.createEmailPasswordSession(email, password);
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+        return parseStringify(newUserAccount);
     } catch (error) {
-        console.error('Error', error);
+        console.error('Error', error); 
     }
 }
