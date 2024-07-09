230f0f568084843095742524cfc820de947afcd8
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-09 11:32:32 +0530

    connect multiple accounts

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -55,7 +55,7 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
                         width={24}
                         height={24}
                     />
-                    <p className='hiddenl text-[16px] font-semibold text-black-2 xl:block'>Connect bank</p>
+                    <p className='hidden text-[16px] font-semibold text-black-2 xl:block'>Connect bank</p>
                 </Button>
             ) : (
                 <Button
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
+import PlaidLink from "./PlaidLink"
 
 const Sidebar = ({ user }: SiderbarProps) => {
     const pathname = usePathname();
@@ -48,6 +49,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                         </Link>
                     )
                 })}
+                <PlaidLink user={user} />
             </nav>
             <Footer user={user} />
         </section>
